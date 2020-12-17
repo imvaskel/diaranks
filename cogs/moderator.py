56e15d74f6158c18c0a05269a67b5b0e1a705d86
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2020-12-16 20:35:59 -0500

    fix add_level_role, hopefully

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -40,7 +40,7 @@ class ModeratorCog(Cog, name = "Moderator"):
         except:
             pass
 
-        await self.bot.db.excecute("INSERT INTO roles(roleId, level) VALUES(?)", (role.id, level))
+        await self.bot.db.excecute("INSERT INTO roles(roleId, level) VALUES(?, ?)", (role.id, level))
 
         self.bot.level_roles.update({level: role.id})
 
