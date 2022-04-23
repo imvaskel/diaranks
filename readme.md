2763cfbfa6dbde6a19cf30517b56fd8491730504
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-04-23 01:07:48 -0400

    fix imports in utils/placard

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Diaranks
+Diaranks is a ranks bot specially made for discord.gg/diabetes, though anyone can use it.
+Note that it is quite hard coded, and it being open source is to allow for contributions from the discord.
--- utils/placard.py
+++ utils/placard.py
@@ -1,5 +1,5 @@
 import discord
-from utils import Levelling
+from .levels import get_level_xp, get_level_from_xp, get_remaining_xp
 from jishaku.functools import executor_function
 from disrank.generator import Generator
 from .bot import Bot
@@ -10,16 +10,16 @@ def generate_placard(
     member: discord.Member, xp: int, bot: Bot, *, bg: str = ""
 ) -> discord.File:
 
-    level = Levelling.get_level_from_xp(xp)
-    remaining_xp = Levelling.get_remaining_xp(xp)
+    level = get_level_from_xp(xp)
+    remaining_xp = get_remaining_xp(xp)
 
     args = {
         "bg_image": bg,
-        "profile_image": member.avatar.with_format("png"),
+        "profile_image": member.display_avatar.with_format("png"),
         "level": level,
         "current_xp": 0,
         "user_xp": remaining_xp,
-        "next_xp": Levelling.get_level_xp(level + 1),
+        "next_xp": get_level_xp(level + 1),
         "user_position": bot.get_user_position(member.id),
         "user_name": str(member),
         "user_status": member.status.name,
