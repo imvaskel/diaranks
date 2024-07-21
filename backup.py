e30f88343aae18a36ac420205c34d273c7ffabb9
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-07-21 04:02:25 +0000

    rewrite backup script to remove backups <7d

--- backup.py
+++ backup.py
@@ -1,17 +1,18 @@
+import datetime
 import pathlib
-from datetime import datetime
+import re
+from datetime import date
 
 import docker
 from docker.models.containers import Container
 from docker.models.networks import Network
 
 
-def make_backup_file_name() -> str:
-    now = datetime.now()
-
-    return f"backup-{now:%m-%d-%y}.sql"
+def make_backup_file_name(date: datetime.date = date.today()) -> str:
+    return f"backup-{date:%m-%d-%y}.sql"
 
 
+NAME_REGEX = re.compile(r"backup-(?P<month>\d+)-(?P<day>\d+)-(?P<year>\d+).sql")
 FOLDER = pathlib.Path(__file__).parent
 NETWORK_NAME = f"{FOLDER.name}_default"
 BACKUP_NAME = make_backup_file_name()
@@ -27,3 +28,18 @@ for container in network.containers:
         with pathlib.Path(FOLDER / "backups" / BACKUP_NAME).open("wb") as fp:
             fp.write(res.output)
         print(f"written to {BACKUP_NAME}")
+
+today = date.today()
+last_week = date.today() - datetime.timedelta(days=7)
+
+print(f"discarding backups older than {last_week:%m-%d-%y}.")
+
+for file in pathlib.Path("backups/").iterdir():
+    if match := NAME_REGEX.match(file.name):
+        day = int(match["day"])
+        month = int(match["month"])
+        year = int(match["year"]) + 2000
+        d = date(year, month, day)
+        if not last_week <= d <= today:
+            print(f"deleting {file.name}")
+            file.unlink()
